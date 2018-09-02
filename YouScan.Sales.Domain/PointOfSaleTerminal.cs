4e18f75f003f4950bac30d1cbfa007f6dfc6466a
Sviatoslav Filippov <filippov.sviatoslav@gmail.com>
2018-09-02 11:32:53 +0300

    scan implementation

--- YouScan.Sales.Domain/PointOfSaleTerminal.cs
+++ YouScan.Sales.Domain/PointOfSaleTerminal.cs
@@ -6,23 +6,24 @@ namespace YouScan.Sales.Domain
     public class PointOfSaleTerminal
     {
         public Pricing Pricing { get; }
+        private List<Product> ScannedProducts { get; }
 
         public PointOfSaleTerminal(Pricing pricing)
         {
             Pricing = pricing ?? throw new ArgumentNullException(nameof(pricing));
+            ScannedProducts = new List<Product>();
         }
 
         public void Scan(Product product)
         {
             if (product == null) throw new ArgumentNullException(nameof(product));
+            ScannedProducts.Add(product);
         }
 
         public void ScanMany(IEnumerable<Product> products)
         {
-            foreach (var product in products)
-            {
-                Scan(product);
-            }
+            if (products == null) throw new ArgumentNullException(nameof(products));
+            ScannedProducts.AddRange(products);
         }
 
         public Money CalculateTotal()
